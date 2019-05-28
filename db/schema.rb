aad55265c99b63b9829ed2c460530af55b845c5f
valcwt <cauwet.valentin@gmail.com>
2019-05-28 12:26:01 +0200

    modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_100729) do
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "details"
-
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
